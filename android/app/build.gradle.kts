e81148992d1ab62945cd0776b7338a8d2affe392
Shubham Gupta <shubhamcmp695@gmail.com>
2026-03-24 15:49:53 +0530

    Add files via upload

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         minSdk = 23
         targetSdk = 34
         versionCode = flutter.versionCode
-        versionName = "1.1.4"
+        versionName = "1.1.6"
     }
     buildTypes {
         release {
